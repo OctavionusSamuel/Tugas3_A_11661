099b41cf9da666fb715e97e355b4af31221e6e60
OctavionusSamuel <220711661@students.uajy.ac.id>
2024-10-05 16:29:04 +0700

    Update Tugas_Modul3_A_SAMUEL_11661.py

--- Tugas_Modul3_A_SAMUEL_11661.py
+++ Tugas_Modul3_A_SAMUEL_11661.py
@@ -73,11 +73,10 @@ if uploaded_file is not None:
     st.write("<h3 style='text-align: center; color: #0073e6;'>Data yang diupload: </h3>", unsafe_allow_html=True)
     st.dataframe(input_data)
 
-    model_directory = r'C:\Users\Asus\Kuliah\Semester 5\PMDPM\Pertemuan 06\Tugas3_A_11661'
-    model_path = os.path. join(model_directory, r'RR_IPK_model.pkl')
+    model = r'RR_IPK_model.pkl'
 
-    if os.path.exists(model_path):
-        with open(model_path, 'rb') as f:
+    if os.path.exists(model):
+        with open(model, 'rb') as f:
             loaded_model = pickle.load(f)
 
         scaler = loaded_model[0]
